780d40bbe07e2cb1a6b9308cfea9976a13e38778
khalid ali <mughalkhalid354@gmail.com>
2025-09-13 21:52:23 +0500

    initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv'
+dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://mughalkhalid354_db_user:rA6b24OLk9GKKohp@cluster0.nrw4wro.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
