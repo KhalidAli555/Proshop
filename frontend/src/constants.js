a4f64f1ba0e4c551f3bfb9d0a1f64888dae0fc41
khalid ali <mughalkhalid354@gmail.com>
2025-09-14 11:06:24 +0500

    add k8s files

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,31 +1,7 @@
 // export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+//   process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 export const BASE_URL = 'http://localhost:5000'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
 export const PAYPAL_URL = '/api/config/paypal';
-
-
-// export const BASE_URL =
-//   window.location.hostname === 'localhost' ? '' : 'http://backend:5000';
-
-// export const PRODUCTS_URL = `${BASE_URL}/api/products`;
-// export const USERS_URL = `${BASE_URL}/api/users`;
-// export const ORDERS_URL = `${BASE_URL}/api/orders`;
-// export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
-
-
-// export const BASE_URL = 'http://localhost:5000'; // local run ke liye
-// export const PRODUCTS_URL = `${BASE_URL}/api/products`;
-// export const USERS_URL = `${BASE_URL}/api/users`;
-// export const ORDERS_URL = `${BASE_URL}/api/orders`;
-// export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
-
-
-// export const BASE_URL = 'http://backend:5000';  // backend container ka name
-// export const PRODUCTS_URL = `${BASE_URL}/api/products`;
-// export const USERS_URL = `${BASE_URL}/api/users`;
-// export const ORDERS_URL = `${BASE_URL}/api/orders`;
-// export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
-
